adf87196af2fe43665df41f3c5fd712d5aabc55f
sooraj <soorajvk135@gmail.com>
2026-09-09 10:50:29 +0530

    create vs

---
+++ vs_command.py
@@ -0,0 +1,13 @@
+"""
+#command for initialising
+git init
+
+command for working tree clean 
+git status
+
+command for adding all changes to staying area
+add -A
+
+command for adding staged files to localrepository
+git commit
+"""
